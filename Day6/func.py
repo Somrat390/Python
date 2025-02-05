58a1d9d17d97319c70109112905955117fcaf191
Somrat390 <167867505+Somrat390@users.noreply.github.com>
2025-02-05 19:57:00 +0600

    Add files via upload

---
+++ Day6/func.py
@@ -0,0 +1,19 @@
+print("Hello")
+num_char = len("Hello")
+print(num_char)
+# len and print here bult in function
+
+def my_function():
+    print("Hello")
+    print("Bye")
+
+my_function()
+
+def my_function():
+    # Do this
+    # Then do this
+    # Finally do this
+    pass
+
+# calling function
+my_function()
